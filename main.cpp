3da3bd7d92672383b8319384ee9e009449a32168
yvonne-yang <yvonne.y.yang@outlook.com>
2020-11-07 16:41:19 -0500

    added sample code, input file and bat file

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    cin >> name;
+    cout << "Hello, " << name << ". Welcome to Codecker" << endl;
+    return (0);
+}
--- run_me.bat
+++ run_me.bat
@@ -2,15 +2,16 @@
 pushd %~dp0
 echo %cd%
 echo Starting code testing script...
-g++ -Wall -std=c++11 parser.cpp Shape.cpp -o parser.exe 
+g++ -Wall -std=c++11 main.cpp -o program.exe 
 IF %errorlevel% == 0 (
 echo Compilation successful!
 echo ##########################################################
-parser.exe < in.txt > out.txt
+program.exe < in.txt > out.txt
 echo Please copy the below text and paste it on CodeDecker, 
 echo or upload the text file named "out.txt"
 echo ##########################################################
 type out.txt
+echo ##########################################################
 )
 pause
 exit
