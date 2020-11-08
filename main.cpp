e508582c9661b1313d9284dffb1029fd6e257987
yvonne-yang <yvonne.y.yang@outlook.com>
2020-11-07 22:46:16 -0500

    added file compare

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 int main()
 {
-    string name;
-    cin >> name;
-    cout << "Hello, " << name << ". Welcome to Codecker" << endl;
+    string name[3];
+    cin >> name[2] >> name[1] >> name[0];
+    for (int i = 0; i < 3; i++)
+        cout << "Hello, " << name [i] << ".\nWelcome to Codecker." << endl;
     return (0);
 }
